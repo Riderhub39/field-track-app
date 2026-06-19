e8c50ea55ecf069e30950b5fd7547044efb8387d
KHACHUN <limkc_wm22@student.tarc.edu.my>
2026-06-19 15:30:21 +0800

    upload

--- lib/widgets/face_camera_controller.dart
+++ lib/widgets/face_camera_controller.dart
@@ -531,7 +531,7 @@ Future<String?> _processCameraImageInIsolate(Map<String, dynamic> data) async {
         
         if (Platform.isIOS) {
           // iOS 前置摄像头在图像流中通常是横着的，强制旋转 270 度 (逆时针 90 度) 摆正
-          rotationAngle = 270; 
+          rotationAngle = 180; 
         }
         
         convertedImage = img.copyRotate(convertedImage, angle: rotationAngle);
